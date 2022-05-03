c77f2f059acef7b82eec63a28dba1eab72c02d4e
Anastasia Verbitskaya <nastaverb@gmail.com>
2022-05-03 22:13:54 +0300

    Добавлена отрисовка 6 карточек из коробки через JS

---
+++ vendor/script.js
@@ -0,0 +1,35 @@
+//Шесть карточек «из коробки»
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const cardTemplate = document.querySelector('#card').content;
+const cards = document.querySelector('.cards');
+initialCards.forEach(function (card) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__name-text').textContent = card.name;
+  cards.append(cardElement);
+});
