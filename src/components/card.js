829023b9d689d80d1d30e7e5f2a7be6df745bd29
Anastasia Verbitskaya <nastaverb@gmail.com>
2022-07-09 14:47:27 +0300

    Исправить функцию создания карточки

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,15 @@
 //Импорт
-import { popupAdd, createForm, photoNameInput, photoLinkInput, cardTemplate, cards, photoFull, photoFullImage, photoFullName, createBtn} from './constants.js' ;
+import { popupAdd, createForm, photoNameInput, photoLinkInput, cardTemplate, cards, photoFull, photoFullImage, photoFullName, createBtn } from './constants.js';
 import { openPopup, closePopup } from './modal.js';
-import {likeCardsApi} from './index.js'
+import { deleteThisCard, LikeThisCard } from './index.js'
 
 //Создание карточки
-function createCard(name, link, likes) {
+function createCard(name, link, likes, cardId, serverLikes, serverMe, itemOwner) {
   const card = cardTemplate.querySelector('.card').cloneNode(true);
   card.querySelector('.card__name-text').textContent = name;
   card.querySelector('.card__image').src = link;
   card.querySelector('.card__image').alt = name;
   card.querySelector('.card__like-count').textContent = likes;
-  //Удаление карточки
-  const removeBtn = card.querySelector('.card__remove-icon');
-  function removeCard() {
-    card.remove();
-  }
-  removeBtn.addEventListener('click', removeCard);
   //Превью карточки
   const cardImage = card.querySelector('.card__image');
   const cardName = card.querySelector('.card__name-text');
@@ -25,14 +19,22 @@ function createCard(name, link, likes) {
     photoFullImage.alt = cardName.textContent;
     photoFullName.textContent = cardName.textContent;
   });
-  //const cardLike = card.querySelector('.card__like');
-  //cardLike.addEventListener('click', function () {
-    //likeCardsApi();
-  //})
-
+  //Отображение лайка карточки
+  showLike(serverLikes, serverMe, card);
+  //Отображение иконки удаления
+  showDeleteIcon(itemOwner, serverMe, card);
+  //Слушатель удаления карточки
+  const removeIcon = card.querySelector('.card__remove-icon');
+  removeIcon.addEventListener('click', function () {
+    deleteThisCard(cardId, card);
+  })
+  //Слушатель лайка карточки
+  const cardLike = card.querySelector('.card__like');
+  cardLike.addEventListener('click', function () {
+    LikeThisCard(serverLikes, serverMe, cardId, card);
+  })
+  //Готовая карточка
   return card;
-
-
 }
 
 //Функция, которая добавляет карточку в DOM
@@ -43,7 +45,23 @@ function renderInitialCards(somecard, somecontainer) {
   somecontainer.append(somecard);
 }
 
+//Функция для отображения лайка карточки
+function showLike(serverLikes, serverMe, card) {
+  const cardLike = card.querySelector('.card__like');
+  if (serverLikes.some((user) => user._id === serverMe)) {
+    cardLike.classList.add('card__like_liked');
+  }
+}
 
+//Функция для отображения иконки удаления
+function showDeleteIcon(itemOwner, serverMe, card) {
+  if (itemOwner !== serverMe) {
+    const removeIcon = card.querySelector('.card__remove-icon');
+    removeIcon.classList.remove('card__remove-icon_active');
+  }
+}
+
+//Функции лайка и дизлайка карточки
 function likeCard(result, myCard) {
   const cardLike = myCard.querySelector('.card__like');
   myCard.querySelector('.card__like-count').textContent = result.likes.length;
@@ -56,5 +74,10 @@ function unlikeCard(result, myCard) {
   cardLike.classList.remove('card__like_liked');
 }
 
+//Функция удаления карточки
+function removeCard(myCard) {
+  myCard.remove();
+}
+
 //Экспорт
-export {createCard, renderCard, renderInitialCards, likeCard, unlikeCard};
+export { createCard, renderCard, renderInitialCards, likeCard, unlikeCard, removeCard };
--- src/components/index.js
+++ src/components/index.js
@@ -6,83 +6,54 @@ import {
   profileDesc, profileInfo, nameInput, jobInput, cardTemplate, cards, photoFull, photoFullImage, photoFullName, avaInput
 } from './constants.js';
 import { showInputError, hideInputError, isValid, hasInvalidInput, toggleButtonState, setEventListeners, enableValidation } from './validate.js';
-import { createCard, renderCard, renderInitialCards, adddLike, likeCard, unlikeCard } from './card.js';
+import { createCard, renderCard, renderInitialCards, adddLike, likeCard, unlikeCard, removeCard } from './card.js';
 import { openPopup, closePopupEsc, closePopup } from './modal.js';
 import { getOneCardAndUser, getResponseData, addNewCard, getInitialCards, getUserInfo, sendUserInfo, getCardsAndUser, addLikeToCard, removeLikefromCard, sendUserAvatar, deleteCard } from './api.js';
 import { renderProfile, renderLoading } from './utils';
+
+//Функция, которая делает запрос на сервер и удаляет карточку
+export function deleteThisCard(cardId, myCard) {
+  deleteCard(cardId)
+    .then(() => {
+      removeCard(myCard);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+//Функция, которая делает запрос на сервер и лайкает карточку
+export function LikeThisCard(myLikes, me, cardId, myCard) {
+  if (myLikes.every((user) => user._id !== me)) {
+    addLikeToCard(cardId)
+      .then((result) => {
+        likeCard(result, myCard);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  } else {
+    removeLikefromCard(cardId)
+      .then((result) => {
+        unlikeCard(result, myCard);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+}
 
 //Загрузка карточек
 getCardsAndUser
   .then((result) => {
     result[0].forEach(function (item) {
-      const myCard = createCard(item.name, item.link, item.likes.length);
-      if (item.owner._id !== result[1]._id) {
-        const removeIcon = myCard.querySelector('.card__remove-icon');
-        removeIcon.classList.remove('card__remove-icon_active');
-      }
-      const cardLike = myCard.querySelector('.card__like');
-      if (item.likes.some((user) => user._id === result[1]._id)) {
-        cardLike.classList.add('card__like_liked');
-      }
-      const removeIcon = myCard.querySelector('.card__remove-icon');
-      removeIcon.addEventListener('click', function () {
-        deleteCard(item._id)
-          .catch((err) => {
-            console.log(err);
-          })
-      })
-
-      function handleLike() {
-        if (item.likes.every((user) => user._id !== result[1]._id)) {
-          addLikeToCard(item._id)
-            .then((result) => {
-              likeCard(result, myCard);
-            })
-            .catch((err) => {
-              console.log(err);
-            })
-        } else {
-          removeLikefromCard(item._id)
-            .then((result) => {
-              unlikeCard(result, myCard);
-            })
-            .catch((err) => {
-              console.log(err);
-            })
-        }
-      }
-
-      cardLike.addEventListener('click', function () {
-        handleLike();
-        //if (item.likes.every((user) => user._id !== result[1]._id)) {
-          //addLikeToCard(item._id)
-            //.then((result) => {
-              //likeCard(result, myCard);
-            //})
-            //.catch((err) => {
-              //console.log(err);
-            //})
-        //} else {
-          //removeLikefromCard(item._id)
-            //.then((result) => {
-              //unlikeCard(result, myCard);
-            //})
-            //.catch((err) => {
-              //console.log(err);
-            //})
-        //}
-      })
+      const myCard = createCard(item.name, item.link, item.likes.length, item._id, item.likes, result[1]._id, item.owner._id);
       renderInitialCards(myCard, cards);
     })
   })
   .catch((err) => {
     console.log(err);
   })
-
-
-
-
-
 
 //Загрузка данных пользователя с сервера
 getUserInfo()
@@ -133,8 +104,8 @@ createForm.addEventListener('submit', function (evt) {
   renderLoading(createForm.querySelector('.popup__btn'), "Сохранение...");
   addNewCard()
     .then((result) => {
-      console.log(result);
-      renderCard(createCard(result.name, result.link, result.likes.length), cards);
+      const myCard = createCard(result.name, result.link, result.likes.length, result._id, result.likes, result._id, result.owner._id);
+      renderCard(myCard, cards);
       createForm.reset();
       createBtn.classList.add('popup__btn_inactive');
       createBtn.disabled = true;
@@ -147,38 +118,6 @@ createForm.addEventListener('submit', function (evt) {
       renderLoading(createForm.querySelector('.popup__btn'), "Создать");
     })
 })
-
-
-//function handleSingleLike(result) {
-  //console.log(result);
-  //if (result[1].likes.every((user) => user._id !== result[0]._id)) {
-    //addLikeToCard(result[1]._id)
-      //.then((result) => {
-        //likeCard(result, myCard);
-      //})
-      //.catch((err) => {
-        //console.log(err);
-      //})
-  //} else {
-    //removeLikefromCard(result[1]._id)
-      //.then((result) => {
-        //unlikeCard(result, myCard);
-      //})
-      //.catch((err) => {
-        //console.log(err);
-      //})
-  //}
-//}
-
-//export function likeCardsApi() {
-//getOneCardAndUser
-  //.then((result) => {
-    //handleSingleLike(result);
-  //})
-  //.catch((err) => {
-    //console.log(err);
-  //})
-//}
 
 //Обработчики
 
