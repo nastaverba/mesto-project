f93916ad4c665163590cdcf1b288ca08b3d5020f
fade_luv <ssklvsk@gmail.com>
2022-07-25 20:03:02 +0300

    Настройка класса Card (ООП)

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,51 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   //Я поменяла точку входа
-  entry: { main: './src/components/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-        publicPath: ''
+  entry: { main: "./src/components/index.js" },
+  optimization: {
+    minimize: false,
   },
-  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
-    open: true
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
+        use: "babel-loader",
+        exclude: "/node_modules/",
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: { importLoaders: 1 }
-        },
-        'postcss-loader']
-      }
-      ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  }),
-  new CleanWebpackPlugin(),
-  new MiniCssExtractPlugin()
-]
- }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
