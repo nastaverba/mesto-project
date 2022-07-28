9e6758979c7462eb4a23d869464c20c962186dae
fade_luv <ssklvsk@gmail.com>
2022-07-28 14:52:32 +0300

    Доработано закрытие на Esc у попапов

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,48 +2,40 @@ export class Popup {
   constructor(selector) {
     this._selector = document.querySelector(selector);
     console.log(this._selector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    console.log(this._selector)
-    this._selector.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose);
+    console.log(this._selector);
+    this._selector.classList.add("popup_opened");
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
-    this._selector.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose);
+    this._selector.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
   _handleEscClose(evt) {
-
-
     if (evt.key === "Escape") {
-      console.log(evt.target);
-      evt.target.classList.remove('popup_opened');
+      console.log(this._selector);
+      this._selector.classList.remove("popup_opened");
     }
   }
-
-
-
-
-
-
 
   setEventListeners() {
     // const addBtn = document.querySelector('.profile__add-btn');
     // addBtn.addEventListener('click', () => {
     //   this.open();
     // });
-    const closeBtn = this._selector.querySelector('.popup__close-icon');
-    closeBtn.addEventListener('click', () => {
+    const closeBtn = this._selector.querySelector(".popup__close-icon");
+    closeBtn.addEventListener("click", () => {
       this.close();
     });
-    this._selector.addEventListener('mousedown', (evt) => {
-      if (evt.target.classList.contains('popup')) {
+    this._selector.addEventListener("mousedown", (evt) => {
+      if (evt.target.classList.contains("popup")) {
         this.close();
       }
     });
   }
 }
-
